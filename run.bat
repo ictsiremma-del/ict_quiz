720d7b2d63e459f3cabf8850adece9e21e842459
ictsiremma-del <ict.siremma@gmail.com>
2026-03-18 23:13:50 +0000

    Remove unnecessary local files

--- run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting Glorious Pearls ICT Quiz System...
-echo.
-echo Open your browser and go to: http://localhost:5000
-echo Teacher dashboard: http://localhost:5000/teacher
-echo.
-python app.py
-pause
